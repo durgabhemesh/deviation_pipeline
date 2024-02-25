ba363899bca87f3b3858a457950b780ab82fb2df
“durgabhemesh.bagu@gmail.com” <durgabhemesh.bagu@gmail.com>
2024-02-25 14:37:44 +0530

    Added new

--- Json_To_Tables.py
+++ Json_To_Tables.py
@@ -1,6 +1,59 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
+data={
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters": [
+		{
+			"id": "1002",
+			"type": "Regular"
+		},
+		{
+			"id": "1002",
+			"type": "Chocolate"
+		},
+		{
+			"id": "1003",
+			"type": "Blueberry"
+		},
+		{
+			"id": "1004",
+			"type": "Devil's Food"
+		}
+	],
+	"topping": [
+		{
+			"id": "5001",
+			"type": "None"
+		},
+		{
+			"id": "5002",
+			"type": "Glazed"
+		},
+		{
+			"id": "5005",
+			"type": "Sugar"
+		},
+		{
+			"id": "5007",
+			"type": "Powdered Sugar"
+		},
+		{
+			"id": "5006",
+			"type": "Chocolate with Sprinkles"
+		},
+		{
+			"id": "5003",
+			"type": "Chocolate"
+		},
+		{
+			"id": "5004",
+			"type": "Maple"
+		}
+	]
+}
 class StoreData:
 
     def __init__(self):
